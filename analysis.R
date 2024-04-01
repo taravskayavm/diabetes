9a77570c1ead48fd3460be8016c869010ce8886d
taravskayavm <taravskayavm@gmail.com>
2024-04-01 22:03:45 +0300

    Сделала отдельный файл для скачивания библиотек

--- analysis.R
+++ analysis.R
@@ -1,19 +1,3 @@
-#install.packages(c("remotes", "tidyverse", "psych", "plotly", "rmarkdown", "skimr"))
-#if (!requireNamespace('BiocManager', quietly = TRUE))
-#  install.packages('BiocManager')
-#BiocManager::install('PCAtools')
-#install.packages("httr")
-#install.packages("ggplot2")
-#library(ggplot2)
-#library(remotes)
-#library(tidyverse)
-#library(psych)
-#library(plotly)
-#library(rmarkdown)
-#library(skimr)
-#library(BiocManager)
-#library(httr)
-
 url <- "https://raw.githubusercontent.com/taravskayavm/diabetes/main/diabetes.xlsx%20-%20Sheet%201.csv"
 
 # Отправка GET-запроса и сохранение файла локально
