133d98334677a221a06bba9334642330bc3a28f7
taravskayavm <taravskayavm@gmail.com>
2024-04-01 21:47:03 +0300

    Скачиваем файл с данным с гитхаба

--- analysis.R
+++ analysis.R
@@ -2,7 +2,7 @@
 #if (!requireNamespace('BiocManager', quietly = TRUE))
 #  install.packages('BiocManager')
 #BiocManager::install('PCAtools')
-
+#install.packages("httr")
 #install.packages("ggplot2")
 #library(ggplot2)
 #library(remotes)
@@ -12,7 +12,16 @@
 #library(rmarkdown)
 #library(skimr)
 #library(BiocManager)
+#library(httr)
 
+url <- "https://raw.githubusercontent.com/taravskayavm/diabetes/main/diabetes.xlsx%20-%20Sheet%201.csv"
 
+# Отправка GET-запроса и сохранение файла локально
+response <- GET(url)
+destination <- "data.csv"
+writeBin(content(response, "raw"), destination)
 
+# Прочтение файла с помощью read.csv
+data <- read.csv("data.csv")
 
+View(data)
