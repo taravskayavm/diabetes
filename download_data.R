dd2f0683efae8da1f9f7416c175ae6e77b761cee
taravskayavm <taravskayavm@gmail.com>
2024-04-01 22:05:51 +0300

    Сделала отдельный файл для скачивания данных с гитхаба

---
+++ download_data.R
@@ -0,0 +1,9 @@
+url <- "https://raw.githubusercontent.com/taravskayavm/diabetes/main/diabetes.xlsx%20-%20Sheet%201.csv"
+
+# Отправка GET-запроса и сохранение файла локально
+response <- GET(url)
+destination <- "data.csv"
+writeBin(content(response, "raw"), destination)
+
+# Прочтение файла с помощью read.csv
+data <- read.csv("data.csv")
