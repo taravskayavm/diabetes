e24839c1e4ace926359a30b3306ea2d10a211933
taravskayavm <taravskayavm@gmail.com>
2024-04-08 15:11:32 +0300

    изменила загрузку данных

--- download_data.R
+++ download_data.R
@@ -1,10 +1,7 @@
 # Получение данных с гитхаба ----------------------------------------------
-url <- "https://raw.githubusercontent.com/taravskayavm/diabetes/main/diabetes.xlsx%20-%20Sheet%201.csv"
+diabetes <- read.csv("https://raw.githubusercontent.com/taravskayavm/diabetes/main/diabetes.xlsx%20-%20Sheet%201.csv")
 
-# Отправка GET-запроса и сохранение файла локально
-response <- GET(url)
-destination <- "data.csv"
-writeBin(content(response, "raw"), destination)
 
-# Прочтение файла с помощью read.csv
-data <- read.csv("data.csv")
+
+
+
