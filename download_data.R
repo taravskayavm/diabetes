cc6e3ffe103c33309235df580a90c285ccbf87dd
taravskayavm <taravskayavm@gmail.com>
2024-04-08 14:03:56 +0300

    доработала организацию проекта

--- download_data.R
+++ download_data.R
@@ -1,3 +1,4 @@
+# Получение данных с гитхаба ----------------------------------------------
 url <- "https://raw.githubusercontent.com/taravskayavm/diabetes/main/diabetes.xlsx%20-%20Sheet%201.csv"
 
 # Отправка GET-запроса и сохранение файла локально
