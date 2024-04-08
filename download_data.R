6a5675ff289c21e025ddb44f2efebe99081944a4
taravskayavm <taravskayavm@gmail.com>
2024-04-08 16:24:00 +0300

    исправила получение данных с гитхаба

--- download_data.R
+++ download_data.R
@@ -1,5 +1,5 @@
 # Получение данных с гитхаба ----------------------------------------------
-diabetes <- read.csv("https://raw.githubusercontent.com/taravskayavm/diabetes/main/diabetes.xlsx%20-%20Sheet%201.csv")
+diabetes <- read.csv("https://raw.githubusercontent.com/taravskayavm/diabetes/main/data/diabetes.xlsx_Sheet_1.csv")
 
 
 
