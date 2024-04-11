41e807eb999980e4f5c3a6b9555a99c41fc3a4af
taravskayavm <taravskayavm@gmail.com>
2024-04-11 14:26:26 +0300

    добавила требования к проекту

--- analysis.R
+++ analysis.R
@@ -1,1 +1,5 @@
 View(diabetes)
+
+
+
+
--- download_data.R
+++ download_data.R
@@ -1,17 +1,18 @@
 # Получение данных с гитхаба ----------------------------------------------
-data <- read.csv("https://raw.githubusercontent.com/taravskayavm/diabetes/main/data/diabetes.xlsx_Sheet_1.csv")
+data <- read.csv("https://raw.githubusercontent.com/taravskayavm/diabetes/main/data/diabetes.xlsx_Sheet_1.csv",
+                 na.strings = c("-", ""))
 
 # Замена запятых на точки в дробных числах
 diabetes <- data %>%
   mutate_at(vars(Urea, HbA1c, Chol, TG, HDL, LDL, VLDL, BMI), ~as.numeric(gsub(",", ".", .)))
 
+# Проверка на наличие пропущенных значений во всем датафрейме
+any(is.na(diabetes))
 
+# Подсчет количества дубликатов во всем датафрейме
+sum(duplicated(diabetes))
 
-
-
-
-
-
+str(diabetes)
 
 
 
